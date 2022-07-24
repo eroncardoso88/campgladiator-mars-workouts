56889a4f920c60d856b8635658a2f2cc60e1cc81
eroncardoso88 <deandradeeron@gmail.com>
2022-07-24 13:29:52 -0300

    added trainers to workoutsdb

--- src/fakeApi/index.ts
+++ src/fakeApi/index.ts
@@ -1,5 +1,6 @@
 import { createDatabase } from './basedb'
 import workoutsData from './initialData'
+import trainersData from './trainers'
 
 export interface Workouts {
   description: string;
@@ -13,11 +14,23 @@ export interface Workouts {
   duration: number
 }
 
-const WorkoutsDB = createDatabase<Workouts>()
+export interface Trainers {
+  name: string;
+  id: string;
+}
+
+export interface WorkoutData extends Workouts {
+  name: string
+}
+
+const WorkoutsDB = createDatabase<WorkoutData>()
 const workoutsDB = new WorkoutsDB()
 
 workoutsData.forEach((workout: Workouts) => {
-  workoutsDB.set(workout)
+  workoutsDB.set({
+    name: trainersData.find(t => t.id === workout.trainerId)?.name || "Highly qualified trainer",
+    ...workout,
+  })
 })
 
 export { workoutsDB }
