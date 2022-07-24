95e6360e85159bd6097772e18369c772a8f39269
eroncardoso88 <deandradeeron@gmail.com>
2022-07-23 23:52:56 -0300

    project prep. vite config; alias resolve scss files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '~@campgladiator': path.join(__dirname, '/node_modules/@campgladiator/')
+    }
+  },
+  css: {
+    //CSS preprocessor
+    preprocessorOptions: {
+      sassLoader: {
+      },
+      scss: {
+        charset: false,
+      },
+    },
+  }
 })
