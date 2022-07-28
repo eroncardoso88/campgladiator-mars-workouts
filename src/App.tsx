b5dc59893759185d1fce153f2b00e9ba6b9b5f60
eroncardoso88 <deandradeeron@gmail.com>
2022-07-28 19:31:40 -0300

    header; layout; added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import { Footer } from "@campgladiator/cgui-core.organisms.footer";
 import "./App.scss";
 import { useEffect, useState } from "react";
-import { workoutsDB } from './fakeApi/index'
+import { workoutsDB } from "./fakeApi/index";
+import { Logo } from "@campgladiator/cgui-core.atoms.logo";
+import { Heading } from "@campgladiator/cgui-core.atoms.typography";
+
 
 function App() {
-
   useEffect(() => {
-    console.log(workoutsDB)
-  }, [])
+    console.log(workoutsDB);
+  }, []);
 
   return (
-    <div>
+    <div className="page">
+      <header className="row">
+        <div className="col logo">
+          <img src="src/assets/campgladiator-logo.png" alt="CampGladiator Logo" width="200px"/>
+        </div>
+        <div className="col title">
+          <Heading type="h3" variation="default" font="gotham">
+            Mars Virtual Workouts
+          </Heading>
+        </div>
+          {/* <Logo type="full" width="200px" fullType="red-white"/> */}
+      </header>
+      <main></main>
       <Footer />
     </div>
   );
